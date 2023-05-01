449f5a784f47fee86397a464a367dc6f5e2d500a
Abhishekkanojiya3 <abhi67562@gmail.com>
2023-05-01 14:47:31 +0530

    backend ready

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 
 const cors = require('cors')
 const path = require('path')
+const helmet = require('helmet')
+const compression = require('compression')
 
 const UserRoute = require('./routes/user')
 const sequelize = require('./util/database')
@@ -62,6 +64,8 @@ app.use(cors({
     origin: ['http://127.0.0.1:5500'],
     credentials: true
 }))
+app.use(helmet())
+app.use(compression())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use('/group', groupRoute)
